12416ccfe46057ed4c730d3ec8cff3da9f273eb7
Ha0-wjhhhhhh <1401377418@qq.com>
2022-11-16 20:05:34 +0800

    Initial commit:backend structure

---
+++ authentication/views.py
@@ -0,0 +1,55 @@
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserLoginForm, RegistrationForm
+from django.http import JsonResponse
+
+
+# Create your views here.
+def login_request(request):
+    # TODO backend return json
+    title = "Login"
+    form = UserLoginForm(request.POST or None)
+    context = {
+        'form': form,
+        'title': title,
+    }
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        login(request, user)
+        # messages.info(request, f"You are now logged in  as {user}")
+        # return redirect('index')
+        return JsonResponse({'foo':'bar'})
+    else:
+        print(form.errors)
+        # messages.error(request, 'Username or Password is Incorrect! ')
+    # return render(request, 'authentication/login.html', context=context)
+    return JsonResponse({'foo':'bar'})
+
+
+
+def signup_request(request):
+    # TODO backend return json
+    title = "Create Account"
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+
+    context = {'form': form, 'title': title}
+    return JsonResponse({'foo':'bar'})
+    # return render(request, 'authentication/signup.html', context=context)
+
+
+def logout_request(request):
+    # TODO backend return json
+    logout(request)
+    # messages.info(request, "Logged out successfully!")
+    # return redirect('index')
+    return JsonResponse({'foo':'bar'})
